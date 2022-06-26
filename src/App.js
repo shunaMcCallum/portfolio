a4aa4e7874ec3be822a958724193fdab5d2d2086
Codeclan_Student <codeclan_student@Codeclan-Students-MacBook-Pro.local>
2022-06-26 17:58:58 +0100

    fixed typo in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './containers/Home';
 
 function App() {
   return (
