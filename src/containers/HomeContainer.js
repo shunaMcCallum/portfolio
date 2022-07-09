fab557585db86f8adbe2413667e6232349cbec42
shunaMcCallum <shuna.mccallum@gmail.com>
2022-07-09 15:10:59 +0100

    animated border colours added to homepage

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -5,11 +5,21 @@ const HomeContainer = () => {
 
     return (
         <div className="page-container">
+            
+            <section className="border-wrapper">
+            <div className="border"></div>
+            </section>
+
             <div className="headers">
                 <img id="profile-picture" src="https://brandyourself.com/blog/wp-content/uploads/linkedin-profile-picture-too-close.png" alt='profile_picture' />
                 <h1>Shuna McCallum</h1>
                 <h3>Software Developer</h3>
             </div>
+
+            <section className="border-wrapper">
+                <div className="border"></div>
+            </section>
+
             <div className="homepage-content">
                 <p>Junior Software Developer, recent graduate of Professional Software Development course at CodeClan.</p>
                 <p>Key Skills include:</p>
