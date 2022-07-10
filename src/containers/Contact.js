c5341c81906ebf194408fa91661b63e629ced531
shunaMcCallum <shuna.mccallum@gmail.com>
2022-07-10 17:57:37 +0100

    contact section content added

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -6,6 +6,9 @@ const Contact = () => {
     return (
         <div className="contact-container">
             <h2>Contact</h2>
+            <p><b>Email: </b>shuna.mccallum@gmail.com</p>
+            <p><b>LinkedIn: </b></p>
+            <p><b>Github Profile: </b></p>
         </div>
     )
 }
